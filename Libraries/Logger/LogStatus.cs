797173b9a43eca144d53536a7b99eb086a4c76b0
Hossein Mansouri <hsnmsri82@gmail.com>
2025-03-22 23:51:21 +0330

    complete logger

---
+++ Libraries/Logger/LogStatus.cs
@@ -0,0 +1,14 @@
+namespace TunnelServer.Libraries.Logger
+{
+    /// <summary>
+    /// Represents different log status levels.
+    /// </summary>
+    enum LogStatus
+    {
+        Log,
+        Debug,
+        Success,
+        Error,
+        Warning,
+    }
+}
--- Libraries/Logger/Logger.cs
+++ Libraries/Logger/Logger.cs
@@ -1,21 +1,12 @@
+using TunnelServer.Libraries.Setting;
+
 namespace TunnelServer.Libraries.Logger
 {
-    /// <summary>
-    /// Represents different log status levels.
-    /// </summary>
-    enum LogStatus
-    {
-        Log,
-        Debug,
-        Success,
-        Error,
-        Warning,
-    }
-
     public class Logger
     {
-        private Dictionary<LogStatus, string> LogStatusTitles;
-        private Dictionary<LogStatus, ConsoleColor> LogStatusColors;
+        private readonly Dictionary<LogStatus, string> _logStatusTitles;
+        private readonly Dictionary<LogStatus, ConsoleColor> _logStatusColors;
+        private SettingLoader? _setting;
 
         /// <summary>
         /// Initializes the logger with predefined log status titles and colors.
@@ -23,7 +14,7 @@ namespace TunnelServer.Libraries.Logger
         public Logger()
         {
             // Initialize Log Status Titles
-            this.LogStatusTitles = new Dictionary<LogStatus, string>
+            this._logStatusTitles = new Dictionary<LogStatus, string>
             {
                 { LogStatus.Debug, "DBUG" },
                 { LogStatus.Log, "LOG" },
@@ -33,7 +24,7 @@ namespace TunnelServer.Libraries.Logger
             };
 
             // Initialize Log Status Colors
-            this.LogStatusColors = new Dictionary<LogStatus, ConsoleColor>
+            this._logStatusColors = new Dictionary<LogStatus, ConsoleColor>
             {
                 { LogStatus.Log, ConsoleColor.Gray },
                 { LogStatus.Debug, ConsoleColor.DarkGray },
@@ -50,9 +41,18 @@ namespace TunnelServer.Libraries.Logger
         /// <param name="logStatus">The log status level.</param>
         private void PrintLog(string text, LogStatus logStatus)
         {
-            Console.ForegroundColor = this.LogStatusColors.GetValueOrDefault(logStatus, ConsoleColor.Gray);
-            Console.WriteLine($"[TunnelServer] {DateTime.Now:yyyy/MM/dd-HH:mm} [{this.LogStatusTitles.GetValueOrDefault(logStatus, "UNKN")}]{(this.LogStatusTitles.GetValueOrDefault(logStatus, "UNKN").Length == 3 ? " " : "")}: {text}");
+            Console.ForegroundColor = this._logStatusColors.GetValueOrDefault(logStatus, ConsoleColor.Gray);
+            Console.WriteLine($"[TunnelServer] {DateTime.Now:yyyy/MM/dd-HH:mm} [{this._logStatusTitles.GetValueOrDefault(logStatus, "UNKN")}]{(this._logStatusTitles.GetValueOrDefault(logStatus, "UNKN").Length == 3 ? " " : "")}: {text}");
             Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Set setting for logger
+        /// </summary>
+        /// <param name="setting"></param>
+        public void Setting(SettingLoader setting)
+        {
+            this._setting = setting;
         }
 
         /// <summary>
@@ -61,6 +61,13 @@ namespace TunnelServer.Libraries.Logger
         /// <param name="message">The debug message to log.</param>
         public void Debug(string message)
         {
+            if ((_setting != null) &&
+            (_setting.Logging != null) &&
+            (_setting.Logging.Level != LogLevel.Debug)
+            )
+            {
+                return;
+            }
             this.PrintLog(message, LogStatus.Debug);
         }
 
@@ -70,6 +77,13 @@ namespace TunnelServer.Libraries.Logger
         /// <param name="message">The log message to log.</param>
         public void Log(string message)
         {
+            if ((_setting != null) &&
+            (_setting.Logging != null) &&
+            (_setting.Logging.Level != LogLevel.Debug)
+            )
+            {
+                return;
+            }
             this.PrintLog(message, LogStatus.Log);
         }
 
@@ -79,6 +93,14 @@ namespace TunnelServer.Libraries.Logger
         /// <param name="message">The error message to log.</param>
         public void Error(string message)
         {
+            if ((_setting != null) &&
+            (_setting.Logging != null) &&
+            (_setting.Logging.Level != LogLevel.Error) &&
+            (_setting.Logging.Level != LogLevel.Debug)
+            )
+            {
+                return;
+            }
             this.PrintLog(message, LogStatus.Error);
         }
 
@@ -88,6 +110,13 @@ namespace TunnelServer.Libraries.Logger
         /// <param name="message">The warning message to log.</param>
         public void Warning(string message)
         {
+            if ((_setting != null) &&
+            (_setting.Logging != null) &&
+            (_setting.Logging.Level != LogLevel.Debug)
+            )
+            {
+                return;
+            }
             this.PrintLog(message, LogStatus.Warning);
         }
 
@@ -97,6 +126,13 @@ namespace TunnelServer.Libraries.Logger
         /// <param name="message">The warning message to log.</param>
         public void Success(string message)
         {
+            if ((_setting != null) &&
+            (_setting.Logging != null) &&
+            (_setting.Logging.Level != LogLevel.Debug)
+            )
+            {
+                return;
+            }
             this.PrintLog(message, LogStatus.Success);
         }
     }
