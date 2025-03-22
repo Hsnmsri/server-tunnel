d555dfac2ba942f41da038367d3b8e0e31a44fa9
Hossein Mansouri <hsnmsri82@gmail.com>
2025-03-22 23:52:14 +0330

    fix bugs

--- Program.cs
+++ Program.cs
@@ -1,91 +1,47 @@
-﻿using System;
-using System.Net;
-using System.Net.Sockets;
-using System.Threading.Tasks;
+﻿using System.Net.Sockets;
+using TunnelServer.Libraries.ClientHandler;
 using TunnelServer.Libraries.Logger;
+using TunnelServer.Libraries.Setting;
+using TunnelServer.Libraries.TcpServer;
 
-class Program
+namespace TunnelServer
 {
-    const string localHost = "0.0.0.0";
-    const int localPort = 1001;
-    const string remoteHost = "167.235.156.113";
-    const int remotePort = 1001;
-
-    static Logger logger = new Logger();
-
-    static async Task Main(string[] args)
+    class Program
     {
-        try
-        {
-            // Create and run tcp socket listener
-            TcpListener listener = new TcpListener(IPAddress.Parse(localHost), localPort);
-            listener.Start();
-            logger.Success($"Tunnel server listening on {localHost}:{localPort} port (Force Stop CTRL + C)");
+        static readonly Logger _logger = new();
+        static SettingLoader? _setting;
 
-            while (true)
-            {
-                // Accept connected tcp client
-                TcpClient client = await listener.AcceptTcpClientAsync();
-
-                // Accept connection on multi threads
-                _ = Task.Run(() => HandleClient(client));
-            }
-        }
-        catch (Exception error)
-        {
-            logger.Error(error.Message);
-        }
-    }
-
-    static async Task HandleClient(TcpClient client)
-    {
-        using (client)
+        static async Task Main(string[] args)
         {
             try
             {
-                logger.Log("New connection created");
-                using TcpClient remoteClient = new TcpClient();
-                await remoteClient.ConnectAsync(remoteHost, remotePort);
-                logger.Success($"Connection to {remoteHost}:{remotePort} created");
+                // Update App Settings
+                _setting = new("settings.json", _logger);
+                _setting.UpdateSettings();
 
-                // Get streams
-                using NetworkStream clientStream = client.GetStream();
-                using NetworkStream remoteStream = remoteClient.GetStream();
+                // Set Logger Setting
+                _logger.Setting(_setting);
 
-                // Parallel run read and write between client and remote client
-                Task t1 = PipeData(clientStream, remoteStream);
-                Task t2 = PipeData(remoteStream, clientStream);
+                // Tcp server
+                TcpServer tcpServer = new(_setting, _logger);
+                tcpServer.Start();
 
-                await Task.WhenAny(t1, t2);
+                await HoldAsync(tcpServer);
             }
-            catch (Exception ex)
+            catch (Exception error)
             {
-                logger.Error($"Error: {ex.Message}");
+                _logger.Error(error.Message);
             }
         }
-    }
 
-    /// <summary>
-    /// Transfer data from input to output
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="output"></param>
-    /// <returns></returns>
-    static async Task PipeData(NetworkStream input, NetworkStream output)
-    {
-        byte[] buffer = new byte[8192];
-        int bytesRead;
-        try
+        static async Task HoldAsync(TcpServer tcpServer)
         {
-            while ((bytesRead = await input.ReadAsync(buffer, 0, buffer.Length)) > 0)
-            {
-                await output.WriteAsync(buffer, 0, bytesRead);
-                await output.FlushAsync();
-            }
-        }
-        catch (Exception ex)
-        {
-            logger.Error($"Problem in transfer data: {ex.Message}");
+            TcpClient tcpClient = await tcpServer.AcceptClientAsync();
+
+            ClientHandler clientHandler = new(tcpClient, _setting!, _logger);
+            _ = Task.Run(() => clientHandler.HandleAsync());
+
+            await HoldAsync(tcpServer);
         }
     }
 }
