63520d245c679eeedfaeac77e7d245c653a4b6eb
Hossein Mansouri <hsnmsri82@gmail.com>
2025-03-22 23:52:56 +0330

    complete obj

--- obj/Debug/net8.0/linux-x64/TunnelServer.AssemblyInfo.cs
+++ obj/Debug/net8.0/linux-x64/TunnelServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TunnelServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a2d610b367faffade62dd4f054330bc68b5173b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa6dad0a0d7065767d3fdf3183d632ce62be629d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TunnelServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TunnelServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
